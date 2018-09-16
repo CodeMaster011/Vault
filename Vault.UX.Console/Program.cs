256b3adf1e676899e8dc7c5694cdad5ed221cfa7
Suman Karan <xpd532@gmail.com>
2018-09-16 11:13:24 +0530

    Add project files.

---
+++ Vault.UX.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vault.UX.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
